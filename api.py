cdc9b51a0d3015050b16c675a642b6ef42b67784
Will Shamblin <will@shamblin.org>
2022-09-16 19:33:44 -0400

    Parser, API retrieval working and datagathering finished

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import geojson
+from typing import Optional
+from json import load, dump
+import os
 
 app = FastAPI()
 
@@ -22,23 +25,90 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-placestoexplore = geojson.load(open("assets/Places_To_Explore_-_WCS/Larger_abandonments.geojson"))
-for feature in placestoexplore.features:
-    print(feature)
+fileprefix = "assets/placestoexplore/"
+categories = {
+    "Bridges": fileprefix+"Bridges.geojson",
+    "Drains": fileprefix+"Drains.geojson",
+    "Everything Else": fileprefix+"Everything_else.geojson",
+    "Larger Abandonments": fileprefix+"Larger_abandonments.geojson",
+    "Mines and Tunnels": fileprefix+"Mines_and_Tunnels.geojson",
+    "Places_of_interest": fileprefix+"Places_of_interest.geojson",
+    "Possibly Active": fileprefix+"Possibly_active.geojson",
+    "Small Abandonments": fileprefix+"Small_abandonments.geojson"
+}
+
+bridges = load(open(fileprefix+"Bridges.geojson"))
+drains = load(open(fileprefix+"Drains.geojson"))
+everything_else = load(open(fileprefix+"Everything_else.geojson"))
+larger_abandonments = load(open("assets/placestoexplore/Larger_abandonments.geojson"))
+mines_and_tunnels = load(open("assets/placestoexplore/Mines_and_Tunnels.geojson"))
+places_of_interest = load(open("assets/placestoexplore/Places_of_interest.geojson"))
+possibly_active = load(open("assets/placestoexplore/Possibly_active.geojson"))
+small_abandonments = load(open("assets/placestoexplore/Small_abandonments.geojson"))
+
+
 
 class MapPost(BaseModel):
+    name: str
+    category: str
+    description: str
     lat: float
     lng: float
     color: str
     special: bool
 
+class MapPut(BaseModel):
+    name: Optional[str] = None
+    category: Optional[str] = None
+    description: Optional[str] = None
+    lat: Optional[float] = None
+    lng: Optional[float] = None
+    color: Optional[str] = None
+    special: Optional[bool] = None
+
 
 @app.post('/add/', status_code=status.HTTP_201_CREATED)
 async def add_point(query: MapPost):
-    # query.lat, query.lng, query.color, query.special
+    if query.category not in categories:
+        raise HTTPException(status_code=400, detail=f"category {query.category} not found")
+
+    with open(categories[query.category], 'r') as f:
+        data = load(f)
+        data.append({
+            "type": "Feature",
+            "properties": {
+                "Name": query.name,
+                "description": query.description,
+                "color": query.description,
+                "special": query.special
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    query.lat,
+                    query.lng
+                ]
+            }
+        },
+        )
+
+    os.remove(categories[query.category])
+    with open(categories[query.category], 'w') as f:
+        dump(data, f, indent=4)
 
     return JSONResponse({"success": "Point added"})
 
+
+@app.put('/edit/', status_code=status.HTTP_200_OK)
+async def edit_point(query: MapPut):
+    # Edit data
+    return JSONResponse({"success": "Point edited"})
+
+
 @app.get('/map/', status_code=status.HTTP_200_OK)
 async def return_data():
+    data = {}
+    for category, filepath in categories.items():
+        data[category] = load(open(filepath))
+
     return JSONResponse({"success": data})
--- parser.py
+++ parser.py
@@ -1,12 +1,48 @@
 from pykml import parser
 from pykml.factory import nsmap
+import os, glob
+from json import load, dumps
 
 namespace = {"ns": nsmap[None]}
+
+placemarks = {}
 
 with open("assets/PlacesToExploreWCS.kml") as f:
     root = parser.parse(f).getroot()
     pms = root.findall(".//ns:Placemark", namespaces=namespace)
 
+    i=0
     for pm in pms:
+        i+=1
         styleurl = str(pm.styleUrl).split("-")
-        print(str(pm.name).strip(), "#"+styleurl[2], "special" if styleurl[1]=="1502" else "")
+        color = "#"+styleurl[2]
+        coordinates = str(pm.Point.coordinates).strip().split(",")
+        special =  True if styleurl[1]=="1502" else False
+        try:
+            description = pm.description
+        except AttributeError:
+            description = ""
+
+        print(coordinates[0] + ", " + coordinates[1])
+
+        print("    " + str(pm.name).strip(), color, special, "\n")
+
+        placemarks[coordinates[0], coordinates[1]] = {"color": color, "special": special}
+
+    print("Total:", i, "\n")
+
+for file in glob.glob('assets/geojson/*.geojson'):
+    print("Found geojson:", file)
+    geojson = load(open(os.path.join(os.getcwd(), file)))
+    items = []
+    for item in geojson["features"]:
+        properties = placemarks[str(item["geometry"]["coordinates"][0]), str(item["geometry"]["coordinates"][1])] # Select from placemarks with same coordinates
+        item["properties"]["color"] = properties["color"]
+        item["properties"]["special"] = properties["special"]
+
+        items.append(item)
+
+    outfile = open("assets/placestoexplore/"+file.split("/")[-1].split(".")[0]+".geojson", "w")
+    json_output = dumps(items, indent=4)
+    outfile.write(json_output)
+    outfile.close()
