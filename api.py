1a3f5b8dde132f5016c6f6cbd7fd00c2695e7b42
Will Shamblin <will@shamblin.org>
2022-09-18 17:57:21 -0400

    Better comments and spacing + API loads from single file

--- api.py
+++ api.py
@@ -23,27 +23,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-fileprefix = "assets/placestoexplore/"
 categories = {
-    "Bridges": fileprefix+"Bridges.geojson",
-    "Drains": fileprefix+"Drains.geojson",
-    "Everything Else": fileprefix+"Everything_else.geojson",
-    "Larger Abandonments": fileprefix+"Larger_abandonments.geojson",
-    "Mines and Tunnels": fileprefix+"Mines_and_Tunnels.geojson",
-    "Places of Interest": fileprefix+"Places_of_interest.geojson",
-    "Possibly Active": fileprefix+"Possibly_active.geojson",
-    "Small Abandonments": fileprefix+"Small_abandonments.geojson"
+    "Bridges": "Bridges",
+    "Drains": "Drains",
+    "Everything Else": "Everything_else",
+    "Larger Abandonments": "Larger_abandonments",
+    "Mines and Tunnels": "Mines_and_Tunnels",
+    "Places of Interest": "Places_of_interest",
+    "Possibly Active": "Possibly_active",
+    "Small Abandonments": "Small_abandonments"
 }
 
-bridges = load(open(fileprefix+"Bridges.geojson"))
-drains = load(open(fileprefix+"Drains.geojson"))
-everything_else = load(open(fileprefix+"Everything_else.geojson"))
-larger_abandonments = load(open("assets/placestoexplore/Larger_abandonments.geojson"))
-mines_and_tunnels = load(open("assets/placestoexplore/Mines_and_Tunnels.geojson"))
-places_of_interest = load(open("assets/placestoexplore/Places_of_interest.geojson"))
-possibly_active = load(open("assets/placestoexplore/Possibly_active.geojson"))
-small_abandonments = load(open("assets/placestoexplore/Small_abandonments.geojson"))
-
+placestoexplore = "assets/placestoexplore/Places_to_Explore.geojson"
 
 
 class MapPost(BaseModel):
@@ -70,7 +61,7 @@ async def add_point(query: MapPost):
     if query.category not in categories:
         raise HTTPException(status_code=400, detail=f"category {query.category} not found")
 
-    with open(categories[query.category], 'r') as f:
+    with open(placestoexplore, 'r') as f:
         data = load(f)
         data.append({
             "type": "Feature",
@@ -78,7 +69,8 @@ async def add_point(query: MapPost):
                 "Name": query.name,
                 "description": query.description,
                 "color": query.description,
-                "special": query.special
+                "special": query.special,
+                "category": categories[query.category]
             },
             "geometry": {
                 "type": "Point",
@@ -90,8 +82,8 @@ async def add_point(query: MapPost):
         },
         )
 
-    os.remove(categories[query.category])
-    with open(categories[query.category], 'w') as f:
+    os.remove(placestoexplore)
+    with open(placestoexplore, 'w') as f:
         dump(data, f, indent=4)
 
     return JSONResponse({"success": "Point added"})
@@ -100,13 +92,12 @@ async def add_point(query: MapPost):
 @app.put('/edit/', status_code=status.HTTP_200_OK)
 async def edit_point(query: MapPut):
     # Edit data
+
     return JSONResponse({"success": "Point edited"})
 
 
 @app.get('/map/', status_code=status.HTTP_200_OK)
 async def return_data():
-    data = {}
-    for category, filepath in categories.items():
-        data[category] = load(open(filepath))
+    data = load(open(placestoexplore))
 
     return JSONResponse({"success": data})
