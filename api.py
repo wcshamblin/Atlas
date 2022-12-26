e96c02a385fd82375b686be6c6800262e95b712c
Will Shamblin <wcshamblin@gmail.com>
2022-12-26 20:50:25 +0000

    Begin adding auth

--- api.py
+++ api.py
@@ -10,6 +10,19 @@ import os
 
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="!secret")
+
+config = Config('.env')
+oauth = OAuth(config)
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
 
 origins = [
     "https://localhost",
