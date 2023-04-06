ba9497222bcc88b01b0a49c19c570a9e498ae401
Will Shamblin <will@shamblin.org>
2023-04-06 16:24:11 -0400

    Start

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-tmux new-session -d -n "Atlas-API" python3 -m uvicorn --workers 4 --port 5000 api:app
+#tmux new-session -d -n "Atlas-API" python3 -m uvicorn --workers 4 --port 5000 api.api:app
+python3 -m uvicorn --workers 4 --port 5000 api.api:app
