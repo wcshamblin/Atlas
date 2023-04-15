2e37dcf971c23f0624003107e602e6fbc1b8e7a2
wcshamblin <wcshamblin@gmail.com>
2023-04-15 13:38:48 -0400

    Test converting towers to triangle polygons for extrusion

---
+++ assets/decom-towers/testpolygons.py
@@ -0,0 +1,35 @@
+from json import load, dump
+
+towers = load(open("decoms.geojson", "r"))
+
+tower_polygons = {
+    "type": "FeatureCollection",
+    "features": []
+}
+
+for tower in towers["features"]:
+    print(tower)
+
+    # make a triangle centered around the coords
+    triangle_top = [tower["geometry"]["coordinates"][0], tower["geometry"]["coordinates"][1] + 0.0001]
+    triangle_left = [tower["geometry"]["coordinates"][0] - 0.0001, tower["geometry"]["coordinates"][1] - 0.0001]
+    triangle_right = [tower["geometry"]["coordinates"][0] + 0.0001, tower["geometry"]["coordinates"][1] - 0.0001]
+
+    tower_poly = {
+        "type": "Feature",
+        "properties": {
+            "name": tower["properties"]["name"],
+            "description": tower["properties"]["description"],
+            "color": tower["properties"]["color"],
+            "height": tower["properties"]["height"]
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[triangle_top, triangle_left, triangle_right, triangle_top]]
+        }
+    }
+
+    tower_polygons["features"].append(tower_poly)
+
+
+dump(tower_polygons, open("testpolygons.geojson", "w"))
