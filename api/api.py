12416d3e221452bead981ac2fb079fd62fa6617e
Will Shamblin <will@shamblin.org>
2023-05-06 22:18:25 -0400

    RabbitEars and height agl

--- api/api.py
+++ api/api.py
@@ -398,6 +398,8 @@ async def get_antennas_nearby(response: Response, lat: float, lng: float, radius
             "name": antenna["facility_id"],
             "description": "",
             "transmitter_type": "tv",
+            "height_agl": float(antenna["height_agl"]),
+            "RabbitEars": antenna["RabbitEars"],
             "erp": float(antenna["max_erp"]),
             "facility_id": antenna["facility_id"],
             "channel": antenna["channel"],
