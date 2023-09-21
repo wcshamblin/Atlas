eed90c2d815a47250e65d842f0dd430f237fa8e6
Will Shamblin <will@shamblin.org>
2023-09-21 10:37:34 -0400

    Simpler now

--- api/parsers/createmapthumbnails.py
+++ api/parsers/createmapthumbnails.py
@@ -1,36 +1,12 @@
 from PIL import Image
 from sys import argv
 
-google_hybrid_path = argv[1]
-bing_hybrid_path = argv[2]
-esri_path = argv[3]
-naip_path = argv[4]
-vfr_path = argv[5]
-openstreetmap_path = argv[6]
+left = 2025
+top = 776
+right = left+60
+bottom = top+60
 
-google_hybrid = Image.open(google_hybrid_path)
-bing_hybrid = Image.open(bing_hybrid_path)
-esri = Image.open(esri_path)
-naip = Image.open(naip_path)
-vfr = Image.open(vfr_path)
-openstreetmap = Image.open(openstreetmap_path)
-
-left = 1994
-top = 720
-right = 1994 + 60
-bottom = 720 + 60
-
-google_hybrid = google_hybrid.crop((left, top, right, bottom))
-bing_hybrid = bing_hybrid.crop((left, top, right, bottom))
-esri = esri.crop((left, top, right, bottom))
-naip = naip.crop((left, top, right, bottom))
-vfr = vfr.crop((left, top, right, bottom))
-openstreetmap = openstreetmap.crop((left, top, right, bottom))
-
-# save the images
-google_hybrid.save(google_hybrid_path)
-bing_hybrid.save(bing_hybrid_path)
-esri.save(esri_path)
-naip.save(naip_path)
-vfr.save(vfr_path)
-openstreetmap.save(openstreetmap_path)
+for arg in argv[1:]:
+    image = Image.open(arg)
+    cropped = image.crop((left, top, right, bottom))
+    cropped.save(arg)
