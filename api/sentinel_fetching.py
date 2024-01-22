256dd6bd17d875e10edfcd20efd6c359bffb021c
Will Shamblin <wcshamblin@gmail.com>
2024-01-22 22:43:44 +0000

    Attempt to fix token expiry issue

--- api/sentinel_fetching.py
+++ api/sentinel_fetching.py
@@ -1,16 +1,15 @@
 import requests
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2.rfc6749.errors import TokenExpiredError
+from time import time
 
 # Your client credentials
 client_id = 'sh-ab15c4e1-32e4-4cd0-8105-18b495d02b0c'
 client_secret = 'tkkIaIzMkaRsaes999isP0u6lJzvpoBi'
 
-# Create a session
 client = BackendApplicationClient(client_id=client_id)
 oauth = OAuth2Session(client=client)
-
-# Get token for the session
 token = oauth.fetch_token(token_url='https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token',
                           client_secret=client_secret, include_client_id=True)
 
@@ -27,9 +26,16 @@ function evaluatePixel(sample) {
   return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02]
 }
 """
+def retrieve_sentinel_image(oauth, url, request):
+    resp = oauth.post(url, json=request)
+    return resp.content
 
 # ISO-8601 formatted time intervals
 def get_sentinel_image(time_range: list, bbox: list):
+    if token['expires_at'] < time():
+        oauth.refresh_token(token_url='https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token',
+                            client_secret=client_secret)
+        
     request = {
         "input": {
             "bounds": {
@@ -63,6 +69,7 @@ def get_sentinel_image(time_range: list, bbox: list):
     url = "https://sh.dataspace.copernicus.eu/api/v1/process"
 
     resp = oauth.post(url, json=request)
+    
     return resp.content
 
 if __name__ == '__main__':
@@ -72,7 +79,7 @@ if __name__ == '__main__':
     
     timerange = ["2024-01-01T00:00:00Z", "2024-01-22T00:00:00Z"]
 
-    image = get_image(bbox, timerange)
+    image = get_sentinel_image(bbox, timerange)
     
     with open("image.png", "wb") as f:
         f.write(image)
