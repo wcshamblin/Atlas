1695d2a2fa7223c64684449cd951ecc8722b6703
wcshamblin <wcshamblin@gmail.com>
2024-02-04 13:32:00 -0500

    Deploy very experimental right click on mobile code

--- website/src/components/map.js
+++ website/src/components/map.js
@@ -78,6 +78,12 @@ const shadeMap = new ShadeMap({
     debug: (msg) => { console.log(new Date().toISOString(), msg) },
 })
 
+// detect mobile browsers
+window.mobileAndTabletCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
 
 function Map() {
     const { getAccessTokenSilently } = useAuth0();
@@ -146,6 +152,9 @@ function Map() {
 
     const settingsRef = useRef({});
     const [settings, setSettings] = useState({});
+
+    // dealing with mobile right clicks
+
 
     const addSources = () => {
         mapbox.current.addSource('Google Hybrid', {
@@ -1058,13 +1067,40 @@ function Map() {
         });
 
         // on right click
-        mapbox.current.on('contextmenu', (e) => {
-            console.log("Right click at: " + e.lngLat.lng + ", " + e.lngLat.lat);
-            // make new popup with coordinates of right click
-            setRightClickPopupPosition([e.lngLat.lng, e.lngLat.lat])
-            setShowRightClickPopup(true);
-            setRightClickPopupState("default");
-        });
+        if (window.mobileAndTabletCheck()) {
+            init_mobile_contextmenu();
+        } else {
+            mapbox.current.on('contextmenu', (e) => {
+                setRightClickPopupPosition([e.lngLat.lng, e.lngLat.lat])
+                setShowRightClickPopup(true);
+                setRightClickPopupState("default");
+            });
+        }
+
+        function init_mobile_contextmenu() {
+            let iosTimeout = null;
+            let clearIosTimeout = () => { clearTimeout(iosTimeout); };
+
+            mapbox.current.on('touchstart', (e) => {
+                if (e.originalEvent.touches.length > 1) {
+                    return;
+                }
+                iosTimeout = setTimeout(() => {
+                    setRightClickPopupPosition([e.lngLat.lng, e.lngLat.lat])
+                    setShowRightClickPopup(true);
+                    setRightClickPopupState("default");
+                }, 250);
+            });
+            mapbox.current.on('touchend', clearIosTimeout);
+            mapbox.current.on('touchcancel', clearIosTimeout);
+            mapbox.current.on('touchmove', clearIosTimeout);
+            mapbox.current.on('pointerdrag', clearIosTimeout);
+            mapbox.current.on('pointermove', clearIosTimeout);
+            mapbox.current.on('moveend', clearIosTimeout);
+            mapbox.current.on('gesturestart', clearIosTimeout);
+            mapbox.current.on('gesturechange', clearIosTimeout);
+            mapbox.current.on('gestureend', clearIosTimeout);
+        }
 
         // on move, if zoom is above 14, try to update towers and / or antennas
         mapbox.current.on('moveend', () => {
